6d3897e078a1d1dd9399d6627d3b793b6b2270b9
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-09-18 13:01:30 -0700

    p

--- _printf.c
+++ _printf.c
@@ -4,13 +4,11 @@
 *@format: format.
 *Return: Printed chars.
 **/
-
 int _printf(const char *format, ...)
 {
-int count = 0;
 va_list args;
 va_start(args, format);
-
+int count = 0;
 if (format == NULL)
 {
 return (-1);
@@ -20,16 +18,13 @@ while (*format != '\0')
 if (*format == '%')
 {
 format++;
-switch (*format)
-{
-case 'c':
+if (*format == 'c')
 {
 char c = va_arg(args, int);
 write(1, &c, 1);
 count++;
 }
-break;
-case 's':
+else if (*format == 's')
 {
 char *str = va_arg(args, char *);
 while (*str != '\0')
@@ -39,26 +34,25 @@ str++;
 count++;
 }
 }
-break;
-case '%':
+else if (*format == '%')
+{
 write(1, "%", 1);
 count++;
-break;
-default:
-write(1, "%", 1);
-write(1, format, 1);
-count += 2;
-break;
-}
 }
 else
 {
+write(1, "%", 1);
+write(1, format, 1;
+count += 2;
+}
+}
+else
+}
 write(1, format, 1);
 count++;
 }
 format++;
 }
-
 va_end(args);
 return (count);
 }
