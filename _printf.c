dc48007a67e232e018ea00c63aee5561c8d9e4e5
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-09-18 10:21:53 -0700

    c

--- _printf.c
+++ _printf.c
@@ -7,75 +7,55 @@
 
 int _printf(const char *format, ...)
 {
+va_list args;
+va_start(args, format);
 
-	int charprinted = 0;
-	int divisor = 1;
-	va_list args_list;
-
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	va_start(args_list, format);
-
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			charprinted++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-			{
-				break;
-			}
-			if (*format == 'c')
-			{
-				char ch = va_arg(args_list, int);
-
-				write(1, &ch, 1);
-			}
-			if (*format == 's')
-			{
-				char *string = va_arg(args_list, char *);
-				int string_len = strlen(string);
-
-				write(1, string, string_len);
-				charprinted += string_len;
-			}
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				charprinted++;
-			}
-			if(*format == '%'){
+int count = 0;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+{
+char c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+}
+break;
+case 's':
+{
+char *str = va_arg(args, char *);
+while (*str != '\0')
+{
+write(1, str, 1);
+str++;
+count++;
+}
+}
+break;
+case '%':
+write(1, "%", 1);
+count++;
+break;
+default:
+write(1, "%", 1);
 write(1, format, 1);
-charprinted++ ;
-}
-if (*format == 'd' || *format == 'i') {
-int num = va_arg(args_list, int);
-if (num < 0) {
-write(1, "-", 1);
-charprinted++;
-num = -num;
-}
-while (num / divisor > 9) {
-divisor *= 10;
-}
-while (divisor > 0) {
-int digit = (num / divisor) % 10;
-char digit_char = '0' + digit;
-write(1, &digit_char, 1);
-charprinted++;
-divisor /= 10;
+count += 2;
+break;
 }
 }
-		}
-		format++;
-	}
-	va_end(args_list);
-	return (charprinted);
+else
+{
+write(1, format, 1);
+count++;
 }
+format++;
+}
+
+va_end(args);
+return (count);
+}
+
