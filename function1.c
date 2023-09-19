241ba1203362e8854c813775a81d693bea3c9ab6
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-09-19 02:35:08 -0700

    c

---
+++ function1.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+*_printf - Printf function
+*@format: format.
+*Return: Printed chars.
+**/
+
+int _printf(const char *format, ...)
+{
+int count = 0;
+va_list args;
+va_start(args, format);
+if (format == NULL)
+{
+return (-1);
+}
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+while (*str != '\0')
+{
+write(1, str, 1);
+str++;
+count++;
+}
+}
+else if (*format == '%')
+{
+write(1, "%", 1);
+count++;
+}
+else
+{
+write(1, "%", 1);
+write(1, format, 1);
+count += 2;
+}
+}
+else
+{
+write(1, format, 1);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
