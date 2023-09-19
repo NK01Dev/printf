8d9815ab2e00366701b38bdbe989f7630f7f8d7e
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-09-19 15:38:37 -0700

    c

--- _printf.c
+++ _printf.c
@@ -13,7 +13,10 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-
+if (!format || (*format == '%' && !*(format + 1)))
+return (-1);
+if (*format == '%' && *(format + 1) == ' ' && !*(format + 2))
+return (-1);
 	while (*format != '\0')
 	{
 		if (*format == '%')
